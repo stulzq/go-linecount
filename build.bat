abff3a8ed3d2b6e351fabb766e8a0ce881db7d0f
stulzq <stulzq@qq.com>
2020-12-10 11:19:41 +0800

    add build script

---
+++ build.bat
@@ -0,0 +1,14 @@
+@echo off
+set glc_version=1.0.0
+
+set GOOS=darwin
+set GOARCH=amd64
+go build -o ./output/glc_%glc_version%_darwin_amd64/glc
+
+set GOOS=windows
+set GOARCH=amd64
+go build  -o ./output/glc_%glc_version%_windows_amd64/glc.exe
+
+set GOOS=linux
+set GOARCH=amd64
+go build  -o ./output/glc_%glc_version%_linux_amd64/glc
